c00b3cbc1ac18eab8d7cf6bcc2bd0dcb63c88f52
Szymon Zieniewicz <szymon.zieniewicz.priv@protonmail.ch>
2023-11-01 16:48:27 +0100

    Added users resource

--- Gemfile
+++ Gemfile
@@ -26,7 +26,8 @@ gem "stimulus-rails"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 gem 'semantic-ui-sass', github: 'doabit/semantic-ui-sass'
-
+gem 'bcrypt', '~> 3.1.7'
+gem 'hirb'
 # Use Redis adapter to run Action Cable in production
 # gem "redis", ">= 4.0.1"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+User.create(username: "Szymon", password: "password")
+User.create(username: "Natalia", password: "password")
+User.create(username: "Michał", password: "password")
+User.create(username: "Darek", password: "password")
+User.create(username: "Ola", password: "password")
+User.create(username: "Kamila", password: "password")

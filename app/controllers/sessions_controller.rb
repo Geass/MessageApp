660cbe2342776cb1f85acf830d52998bf64480e3
Szymon Zieniewicz <szymon.zieniewicz.priv@protonmail.ch>
2023-11-02 10:06:22 +0100

    added sign in whole path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,32 @@ class SessionsController < ApplicationController
     def app
         @messages = Message.all
     end
-end
+
+  
+
+    def new
+    end
+    
+      def create
+        user = User.find_by(username: params[:session][:username])
+        if user && user.authenticate(params[:session][:password])
+          session[:user_id] = user.id
+          flash[:success] = "You have successfully logged in"
+          redirect_to app_path
+        else
+          flash.now[:error] = "There was something wrong with your login information"
+          render 'new'
+        end
+      end
+    
+      def destroy
+        session[:user_id] = nil
+        flash[:success] = "You have successfully logged out"
+        redirect_to home_path
+      end
+      
+    
+    end
+  
+
   
